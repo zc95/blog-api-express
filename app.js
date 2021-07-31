68a382ab70f68e4da5819f57caa729046c1280cd
zhangcheng <571346800@qq.com>
2021-07-31 12:48:20 +0800

    错误处理

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const app = express();
 const figlet = require('figlet');
 const chalk = require('chalk');
 const path = require('path');
+const dayjs = require('dayjs');
 
 // 初始化数据库
 require('./utils/db');
+
+// 渲染静态资源
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 对传入的请求体进行解析
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 渲染静态资源
-app.use(express.static(path.join(__dirname, 'public')));
+// 请求中间件
+app.use("/", (req, res, next) => {
+    console.log(chalk.cyan(`[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${req.url} 请求信息：`));
+    console.log(req);
+    next();
+});
 
 // 路由中间件
 app.use('/api/test', require('./routes/test'));
 app.use('/api/post', require('./routes/post'));
+
+// 404
+// app.use(function(req, res) {
+//     // res.send('404');
+// });
+
+// 错误处理中间件
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).json({
+        message : err.message,
+        errors  : err.stack
+    });
+});
 
 // 炫酷但没啥用的文字工具
 figlet('blog-api', (err, data) => console.log(data));
--- routes/test.js
+++ routes/test.js
@@ -1,12 +1,7 @@
 const express = require('express');
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/get-api', (req, res) => {
-    console.log(req.query);
-    res.send({
-        code : 0,
-        msg  : '测试get请求'
-    });
+router.get('/', (req, res, next) => {
+    return next(new Error('qqq'));
 });
 module.exports = router;
